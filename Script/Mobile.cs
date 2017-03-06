a720f5ad23f3ca6ca2aa00b4418b0a2c0113b3bf
Mohamed Galal <abogalalmedo@gmail.com>
2017-03-06 05:22:15 +0200

    Script
    another way to movement

---
+++ Script/Mobile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mobile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       // transform.Rotate(0, Input.acceleration.y * 10f * Time.deltaTime, 0);
+        //Input.acceleration.x * Time.deltaTime * 10f
+      //  transform.Rotate(Input.acceleration.x * Time.deltaTime * 10f, 0, 0);
+
+
+        //Input.acceleration.y * 10f * Time.deltaTime
+
+       transform.Rotate(0, 0, Input.acceleration.y * 10f * Time.deltaTime);
+	}
+}
